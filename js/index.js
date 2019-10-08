1f6fb8566a54978c490b3b9c15d907073273c061
Sam <samuelpuckett77@gmail.com>
2019-10-07 22:58:26 -0700

    Programmatically Add Items
    - Figured out how to programatically add items using the same technique I used from TechCrunch's Project.
    - Added a: Melee, Ultimate, and Sticker page
    - Made the Melee and Ultiamte pages work and only display their respective items.
    - Made the stickers page a coming soon landing page.
    - Made the home display the newest items.
    - Now to add items, I only have to update the merch.js file.

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i<arr.length; i++) {
+      var merchName = arr[i].name.toString();
+      var merchTitle = arr[i].title.toString();
+      var merchPhoto = arr[i].photo.toString();
+      var merchLink = arr[i].link.toString();
+    //   var merchGame = tempCo2[1] + ', ' + tempCo2[0];
+      var merchDescription = arr[i].description.toString();
+      var merchPrice = arr[i].price.toString();
+    //   var merchOrder = tempCo2[1] + ', ' + tempCo2[0];
+      //console.log(tempCo3);
+
+      out += '<div class="col-lg-4 col-md-6 mb-4"> <div class="card h-100"> <a href="' + merchLink + '"><img class="card-img-top" src="' + merchPhoto + '" alt=""></a> <div class="card-body"> <h4 class="card-title"> <a href="' + merchLink + '">' + merchTitle + '</a> </h4> <h5>' + merchPrice + '</h5> <p class="card-text">' + merchDescription + '</p> </div> <div class="card-footer"> <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9733;</small> </div> </div></div>';;
+    }
+    document.getElementById("items").innerHTML = out;
+  }
+
+  myFunction(myArray);
--- js/merch.js
+++ js/merch.js
@@ -7,7 +7,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41592010-pk-fiya?p=classic-tee&asc=u",
         "game": "ultimate",
         "description": "Show off your moves in this 'IGOR' style Ness T-Shirt!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "14"
     },
 
     {
@@ -17,7 +18,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/34945417-fox-mccloud-smash-bros-rare-vintage-shirt?p=classic-tee&ref=available_products_swiper",
         "game": "melee",
         "description": "Do you main Fox McCloud in Smash Brothers Melee for the Gamecube!? While this shirt won't make you a better player, it will make you look a lot cooler if you lose!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "13"
     }, 
 
     {
@@ -27,7 +29,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41451761-you-arent-worth-the-trouble-20xx-falco-smash-melee?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=34945417",
         "game": "melee",
         "description": "Do you main Falco in super smash bros melee for the nintendo gamecube? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "12"
     },
 
     {
@@ -37,7 +40,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41220276-simon-belmont-vanquish-the-darkness?asc=u&carousel_pos=13&p=classic-tee&ref=portfolio_product_carousel&ref_id=41451761",
         "game": "ultimate",
         "description": "Do you main Simon Belmont in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "11"
     },
 
     {
@@ -47,7 +51,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/36457919-young-link-smash-ultimate-lean?asc=u&carousel_pos=8&p=classic-tee&ref=portfolio_product_carousel&ref_id=41592010",
         "game": "ultimate",
         "description": "Get yourself this super soft shirt to show off your love for young link and smash bros ultimate for the nintendo switch!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "10"
     },
 
     {
@@ -57,7 +62,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41185249-king-dedede-studio-ghibli?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=36457919",
         "game": "ultimate",
         "description": "Do you main King Dedede in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "9"
     },
 
     {
@@ -67,7 +73,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41452257-kibry-sees-ghosts?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
         "game": "ultimate",
         "description": "Do you main Kibry in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "8"
     },
 
     {
@@ -77,7 +84,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/41180679-banjo-kazooie-srirachia-hot-sauce?asc=u&carousel_pos=2&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
         "game": "ultimate",
         "description": "Do you main Banjo-Kazooie in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "7"
     },
 
     {
@@ -87,7 +95,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/36458753-ridley-smash-bros-ultimate-metroid?asc=u&carousel_pos=8&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
         "game": "ultimate",
         "description": "If you are a Ridley main in Smash Brothers Ultimate for the nintendo switch this is your shirt!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "6"
     },
 
     {
@@ -97,7 +106,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/36831778-wolf-gang-smash-bros-ultimate?asc=u&carousel_pos=6&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
         "game": "ultimate",
         "description": "Do you main Wolf in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "5"
     },
 
     {
@@ -107,7 +117,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/40848611-hero-dragon-quest-smash-bros?asc=u&carousel_pos=12&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
         "game": "ultimate",
         "description": "Do you main Hero in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "4"
     },
 
     {
@@ -117,7 +128,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/35109116-ness-smash-brothers-melee-earthbound?asc=u&carousel_pos=9&p=classic-tee&ref=similar_designs&ref_id=36831778",
         "game": "melee",
         "description": "Style on your friends in Super Smash Brothers Melee for the Nintendo Gamecube with the double cup ness fit! Inspired by bootleg rapper shirts from back in the day! May not be able to increase the knock-back of your PK Fire, but, will definitely increase your street cred!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "3"
     },
 
     {
@@ -127,7 +139,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/34971270-captain-falcon-smash-bros-melee-rapper-tee?asc=u&carousel_pos=9&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
         "game": "melee",
         "description": "Want to style on your friends both in game and out of game? This shirt is for you! If you're a smash bros melee player for the nintendo gamecube and play falcon, look no further!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "2"
     },
 
     {
@@ -137,7 +150,8 @@ var myArray = [
         "link": "https://www.redbubble.com/people/samwisegamg/works/34935534-captain-falcon-f-zero-vintage?asc=u&carousel_pos=16&p=classic-tee&ref=similar_designs&ref_id=36831778",
         "game": "melee",
         "description": "Show off your moves in this vintage style Captain Falcon T-Shirt!",
-        "price": "$19.99"
+        "price": "$19.99",
+        "order": "1"
     },
 
 ];
