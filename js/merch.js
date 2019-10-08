09d85bafbc47d892af226070adc184e36d868c9e
Sam <samuelpuckett77@gmail.com>
2019-10-07 20:52:44 -0700

    Created array of merch
    - Created array of merch
    - Going to start to serve up the items with the objects so that I don't have to mess with the html whenever I want to add a new item and that way it will be easier to have items be sorted by game and characters in the future.

---
+++ js/merch.js
@@ -0,0 +1,143 @@
+var myArray = [
+
+    {
+        "name": "ness-ultimate",
+        "title": "PK Fiya",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471426/Ness-Shirt-White_hiuyix.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41592010-pk-fiya?p=classic-tee&asc=u",
+        "game": "ultimate",
+        "description": "Show off your moves in this 'IGOR' style Ness T-Shirt!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "fox-melee",
+        "title": "Fox Shirt",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471431/Fox_Shirt_hajyd9.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/34945417-fox-mccloud-smash-bros-rare-vintage-shirt?p=classic-tee&ref=available_products_swiper",
+        "game": "melee",
+        "description": "Do you main Fox McCloud in Smash Brothers Melee for the Gamecube!? While this shirt won't make you a better player, it will make you look a lot cooler if you lose!",
+        "price": "$19.99"
+    }, 
+
+    {
+        "name": "falco-melee",
+        "title": "You Aren't Worth The Trouble",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471420/falco-shirt_pzdbvx.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41451761-you-arent-worth-the-trouble-20xx-falco-smash-melee?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=34945417",
+        "game": "melee",
+        "description": "Do you main Falco in super smash bros melee for the nintendo gamecube? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "simon-ultimate",
+        "title": "Vanquish The Darkness",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471431/Tshirt_mockup_3_xam4a5.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41220276-simon-belmont-vanquish-the-darkness?asc=u&carousel_pos=13&p=classic-tee&ref=portfolio_product_carousel&ref_id=41451761",
+        "game": "ultimate",
+        "description": "Do you main Simon Belmont in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "young-link-ultimate",
+        "title": "Yung Leank",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471413/Yink_Shirt_g38ne4.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/36457919-young-link-smash-ultimate-lean?asc=u&carousel_pos=8&p=classic-tee&ref=portfolio_product_carousel&ref_id=41592010",
+        "game": "ultimate",
+        "description": "Get yourself this super soft shirt to show off your love for young link and smash bros ultimate for the nintendo switch!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "king-dedede-ultimate",
+        "title": "The King",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471422/DEDEDE_Preview_yytdry.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41185249-king-dedede-studio-ghibli?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=36457919",
+        "game": "ultimate",
+        "description": "Do you main King Dedede in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "kirby-ultimate",
+        "title": "Kirby Sees Ghosts",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570471432/kirby-shirt_xzte1k.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41452257-kibry-sees-ghosts?asc=u&carousel_pos=1&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
+        "game": "ultimate",
+        "description": "Do you main Kibry in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "banjo-ultimate",
+        "title": "Banjo Sauce",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570495570/Tshirt_mockup_2_bidk9i.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/41180679-banjo-kazooie-srirachia-hot-sauce?asc=u&carousel_pos=2&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
+        "game": "ultimate",
+        "description": "Do you main Banjo-Kazooie in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "ridley-ultimate",
+        "title": "Ridley Sauce",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501019/Ridley-Shirt_vhvrp0.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/36458753-ridley-smash-bros-ultimate-metroid?asc=u&carousel_pos=8&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
+        "game": "ultimate",
+        "description": "If you are a Ridley main in Smash Brothers Ultimate for the nintendo switch this is your shirt!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "wolf-ultimate",
+        "title": "Wolf Gang",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501027/Wolf-Shirt_rlspon.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/36831778-wolf-gang-smash-bros-ultimate?asc=u&carousel_pos=6&p=classic-tee&ref=portfolio_product_carousel&ref_id=41185249",
+        "game": "ultimate",
+        "description": "Do you main Wolf in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "hero-ultimate",
+        "title": "RNG God",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501038/Hero-Shirt_l7gcir.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/40848611-hero-dragon-quest-smash-bros?asc=u&carousel_pos=12&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
+        "game": "ultimate",
+        "description": "Do you main Hero in super smash bros ultimate for the nintendo switch? Put on this shirt while you stomp on your opponents or even look stylish while you go 0-2 in pools at your locals!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "ness-melee",
+        "title": "Ness Rapper",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501055/Ness-Shirt-Rapper_k7y7hd.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/35109116-ness-smash-brothers-melee-earthbound?asc=u&carousel_pos=9&p=classic-tee&ref=similar_designs&ref_id=36831778",
+        "game": "melee",
+        "description": "Style on your friends in Super Smash Brothers Melee for the Nintendo Gamecube with the double cup ness fit! Inspired by bootleg rapper shirts from back in the day! May not be able to increase the knock-back of your PK Fire, but, will definitely increase your street cred!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "falcon-rapper-melee",
+        "title": "Falcon Rapper",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501066/falcon-rapper-shirt_ivtery.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/34971270-captain-falcon-smash-bros-melee-rapper-tee?asc=u&carousel_pos=9&p=classic-tee&ref=portfolio_product_carousel&ref_id=36831778",
+        "game": "melee",
+        "description": "Want to style on your friends both in game and out of game? This shirt is for you! If you're a smash bros melee player for the nintendo gamecube and play falcon, look no further!",
+        "price": "$19.99"
+    },
+
+    {
+        "name": "falcon-japanese-melee",
+        "title": "Falcon Japanese",
+        "photo": "https://res.cloudinary.com/dnrbgmcm4/image/upload/c_scale,q_50,w_500/v1570501083/Falcon-Shirt-Japanese_ing8si.png",
+        "link": "https://www.redbubble.com/people/samwisegamg/works/34935534-captain-falcon-f-zero-vintage?asc=u&carousel_pos=16&p=classic-tee&ref=similar_designs&ref_id=36831778",
+        "game": "melee",
+        "description": "Show off your moves in this vintage style Captain Falcon T-Shirt!",
+        "price": "$19.99"
+    },
+
+];
